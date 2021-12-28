959d0cb34528168259eee5fbe88859d8be5071b2
alexandretoullec <69418272+alexandretoullec@users.noreply.github.com>
2021-12-28 13:27:56 +0100

    Add files via upload

---
+++ 3. Pokedex/app.js
@@ -0,0 +1,30 @@
+const searchInput = document.querySelector('.recherche-poke input' );
+
+
+
+
+function fetchPokemonBase () {
+
+    fetch("https://pokeapi.co/api/v2/pokemon?limit=151")
+        .then(reponse => reponse.json())
+        .then(allPoke => {
+            console.log(allPoke);
+        })
+
+}
+
+fetchPokemonBase();
+
+
+
+
+
+// animation Input
+
+searchInput.addEventListener('input', function(e){
+    if(e.target.value !== ""){
+        e.target.parentNode.classList.add('active-input')
+;    }else if(e.target.value === ""){
+        e.target.parentNode.classList.remove('active-input')
+    }
+})
