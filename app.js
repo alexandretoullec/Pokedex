96f426ebbbbdc85589b06b9daad8af6cc7aaec83
alexandretoullec <69418272+alexandretoullec@users.noreply.github.com>
2021-12-29 13:58:31 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
 const searchInput = document.querySelector('.recherche-poke input' );
 let allPokemon = [];
 let tabFin = [];
+const listePoke = document.querySelector('.liste-poke')
 
-
+const types = {
+    grass: '#78c850',
+	ground: '#E2BF65',
+	dragon: '#6F35FC',
+	fire: '#F58271',
+	electric: '#F7D02C',
+	fairy: '#D685AD',
+	poison: '#966DA3',
+	bug: '#B3F594',
+	water: '#6390F0',
+	normal: '#D9D5D8',
+	psychic: '#F95587',
+	flying: '#A98FF3',
+	fighting: '#C25956',
+    rock: '#B6A136',
+    ghost: '#735797',
+    ice: '#96D9D6'
+};
 
 function fetchPokemonBase () {
 
@@ -29,12 +47,76 @@ function fetchPokemonComplet(pokemon) {
     fetch(url)
     .then(reponse => reponse.json())
     .then((pokeData) => {
-        console.log(pokeData);
+        // console.log(pokeData);
+
+        objPokemonComplet.pic = pokeData.sprites.front_default;
+        objPokemonComplet.type = pokeData.types[0].type.name;
+        objPokemonComplet.id = pokeData.id;
+
+fetch(`https://pokeapi.co/api/v2/pokemon-species/${nameP}`)
+.then(reponse => reponse.json())
+.then((pokeData) => {
+    // console.log(pokeData);
+
+    objPokemonComplet.name = pokeData.names[4].name;
+    allPokemon.push(objPokemonComplet);
+
+    if( allPokemon.length === 151){
+        // console.log(allPokemon);
+
+        tabFin = allPokemon.sort( (a,b) =>{
+            return a.id - b.id
+            
+        }).slice(0,21)
+        // console.log(tabFin);
+        createCard(tabFin)
+    }
+    
+})
+
     })
 
 }
 
 
+// création des cartes
+
+function createCard(arr) {
+
+    for(let i = 0 ; i<arr.length ; i++){
+
+        const carte = document.createElement('li');
+
+        let couleur = types[arr[i].type];
+        carte.style.background = couleur;
+        
+        const txtCarte = document.createElement('h5');
+        txtCarte.innerText = arr[i].name;
+        const idCarte = document.createElement;
+        idCarte.innerText = `ID# ${arr[i].id}`;
+        const imgCarte = document.createElement('img');
+        imgCarte.src = arr[i].pic;
+        
+        carte.appendChild(imgCarte);
+        carte.appendChild(txtCarte);
+        // carte.appendChild(idCarte);
+
+        listePoke.appendChild(carte);
+
+    }
+
+}
+
+//Scroll infini
+
+window.addEventListener('scroll',()=>{
+    const {scrollTop, scrollHeight, clientHeight} =  document.documentElement;
+    // scrolTop = scroll depuis le top
+    // scrollHeight = scroll total
+    // clientHeight = hauteur de la fenetre, partir visible.
+    console.log(scrollTop, scrollHeight, clientHeight);
+
+})
 
 // animation Input
 
